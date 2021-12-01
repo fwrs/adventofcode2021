09f3a05a47d59cb8d0effdb3a652527c924198a1
Ilyusha K <ilyusha_k@icloud.com>
2021-12-01 13:05:03 +0300

    Switch to global extensions

--- AdventOfCode2021/Day.swift
+++ AdventOfCode2021/Day.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 protocol Day {
     static func execute()
 }
---
+++ AdventOfCode2021/GlobalExtensions.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+// Safe indexing
+extension Collection {
+    subscript(safe index: Index) -> Element? {
+        indices.contains(index) ? self[index] : nil
+    }
+}
+
+// Safe subranging
+extension Array {
+    subscript(safe range: Range<Index>) -> ArraySlice<Element>? {
+        if range.endIndex > endIndex {
+            if range.startIndex >= endIndex {
+                return nil
+            }
+            return self[range.startIndex..<endIndex]
+        }
+        if range.startIndex < 0 {
+            return nil
+        }
+        return self[range]
+    }
+}
+
+// Sum shorthand
+extension Array where Element == Int {
+    var sum: Int { reduce(0, +) }
+}
+
+// Convert Bool to Int easily
+extension ExpressibleByIntegerLiteral {
+    init(_ booleanLiteral: BooleanLiteralType) {
+        self = booleanLiteral ? 1 : 0
+    }
+}
