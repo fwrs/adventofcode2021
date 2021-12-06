af2ff740006d1a0946b7c76b135f2bb44be7c371
Ilyusha K <ilyusha_k@icloud.com>
2021-12-06 13:04:38 +0300

    Switch to reduce

--- AdventOfCode2021/Day6.swift
+++ AdventOfCode2021/Day6.swift
@@ -1,27 +1,24 @@
 import Foundation
 
 fileprivate extension Array where Element == Int {
-    static let newbornHealth = 8
-    static let postbirthHealth = 6
+    static let newbornCooldown = 8
+    static let postbirthCooldown = 6
     
     func iterateLanternfish(iterationCount: Int) -> Int {
-        var mapping = Array(repeating: 0, count: Self.newbornHealth + 1)
-        
-        for health in self {
-            mapping[health] += 1
+        let mapping = reduce(into: Array(repeating: 0, count: Self.newbornCooldown + 1)) { result, health in
+            result[health] += 1
         }
         
-        for _ in 0..<iterationCount {
-            mapping.append(mapping.removeFirst())
-            mapping[Self.postbirthHealth] += mapping[Self.newbornHealth]
-        }
-        
-        return mapping.sum
+        return (0..<iterationCount).reduce(mapping) { result, _ in
+            Array(result.suffix(from: 1) + result.prefix(1))
+                .enumerated()
+                .map { $0.element + ($0.offset == Self.postbirthCooldown ? result[0] : 0) }
+        }.sum
     }
 }
 
 struct Day6: Day {
-    static let inputData = "<#Fill in the input data here.#>"
+    static let inputData = "3,4,3,1,2"
 
     static func execute() {
         let parsedData = inputData.components(separatedBy: .punctuationCharacters).compactMap(Int.init)
